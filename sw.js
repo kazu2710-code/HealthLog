56fffd6149c66ce324bfd215c61394d175cdd3b5
kazu2710-code <kazu2710@gmail.com>
2026-06-27 15:52:09 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'healthlog-v1';
+const ASSETS = [
+  '/HealthLog/healthlog.html',
+  '/HealthLog/manifest.json',
+  '/HealthLog/icon192x192.png',
+  '/HealthLog/icon512x512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
